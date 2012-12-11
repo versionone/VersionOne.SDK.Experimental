3b77a4fe2d939892365575d1b95db8321771b22d
jogoshugh <jgough@JGough.corp.versionone.net>
2012-12-11 11:00:19 -0500

    Request.Filter + plugin loader transformation basic case working

--- ApiInputTranslatorPlugins/VersionOne.Web.Plugins/Api/ApiTranslatorFilter.cs
+++ ApiInputTranslatorPlugins/VersionOne.Web.Plugins/Api/ApiTranslatorFilter.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Text;
 using System.Web;
+using System.Xml.XPath;
+using VersionOne.Web.Plugins.Composition;
 
 namespace VersionOne.Web.Plugins.Api
 {
@@ -13,12 +15,13 @@ namespace VersionOne.Web.Plugins.Api
 
         public void Init(HttpApplication context)
         {
-            context.BeginRequest += new EventHandler(context_BeginRequest);
+            context.BeginRequest += context_BeginRequest;
         }
 
         void context_BeginRequest(object sender, EventArgs e)
         {
-            HttpContext.Current.Request.Filter = new ApiInputTranslatorFilter(HttpContext.Current.Request.Filter);
+            HttpContext.Current.Request.Filter =
+                new ApiInputTranslatorFilter(HttpContext.Current.Request.Filter);
         }
     }
 
@@ -59,36 +62,68 @@ namespace VersionOne.Web.Plugins.Api
 
         private byte[] _newBuffer = new byte[] { };
 
+        /*
+         * In the case where the input stream total is less than 8092,
+         * then I think this will work fine.
+         * 
+         * If it's greater, we might be able to read the entire InputStream
+         * into a MemoryStream, process it in one swoop, but then
+         * chunk out the translated result in multiple reads???
+         * 
+         * 
+         */
+
+        private ITranslateApiInputToAssetXml GetTranslatorForContentType(string contentType)
+        {
+            var path = HttpContext.Current.Server.MapPath("bin\\Plugins");
+            var inputStreamTranslators = new PartsList<ITranslateApiInputToAssetXml>(path);
+
+            foreach (var translator in inputStreamTranslators.Items)
+            {
+                if (translator.CanTranslate(contentType))
+                {
+                    return translator;
+                }
+            }
+            return null;
+        }
+
         public override int Read(byte[] buffer, int offset, int count)
         {
-            var context = HttpContext.Current;
-            var accept = context.Request.Headers["Accept"];
-
-            if (accept.Equals("upper"))
+            var bytesRead = _sink.Read(buffer, offset, count);
+            if (bytesRead == 0)
             {
-                var bytesRead = _sink.Read(buffer, offset, count);
+                return 0;
+            }
 
-                if (bytesRead == 0)
-                {
-                    return 0;
-                }
+            var context = HttpContext.Current;
+            var contentType = context.Request.Headers["Content-Type"];
 
-                var newContent =
-@"<Asset>
-	<Attribute name=""Phone"" act=""set"">777-666-888-99</Attribute>
-</Asset>";
+            var translator = GetTranslatorForContentType(contentType);
+            if (translator != null)
+            {
+                var content = Encoding.UTF8.GetString(buffer, offset, bytesRead);
 
-                _newBuffer = Encoding.UTF8.GetBytes(newContent);
-                var newBufferByteCountLength = Encoding.UTF8.GetByteCount(newContent);
+                var xml = translator.Execute(content);
 
-                Encoding.UTF8.GetBytes(newContent,
-                                       0, Encoding.UTF8.GetByteCount(newContent), buffer, 0);
+                var translatedContent = xml.CreateNavigator().OuterXml;
+
+                _newBuffer = Encoding.UTF8.GetBytes(translatedContent);
+                var newBufferByteCountLength = Encoding.UTF8.GetByteCount(translatedContent);
+
+                Encoding.UTF8.GetBytes(translatedContent,
+                                       0, newBufferByteCountLength, buffer, 0);
+
+                //if (newBufferByteCountLength > bytesRead)
+                //{
+                //    _sink.Position = newBufferByteCountLength;
+                //}
 
                 return newBufferByteCountLength;
             }
             else
             {
-                return _sink.Read(buffer, offset, count);
+                return bytesRead;
             }
         }
 
